a7aed1cda735f0eeedd0613a41cd0952882d38e4
Gabriele Pompa <gabriele.pompa@symmys.com>
2018-09-15 21:39:53 +0100

    Chap 4 - global variables summary chart and string example added

---
+++ Chap_4/globalVariables_SummaryChart.cpp
@@ -0,0 +1,17 @@
+// Uninitialized definition:
+int g_x;        // defines uninitialized global variable (external linkage, default since non-const)
+static int g_x; // defines uninitialized static global variable (internal linkage)
+const int g_x;  // not allowed: const variables must be initialized
+
+// Forward declaration via extern keyword:
+extern int g_z;       // forward declaration for global variable defined elsewhere
+extern const int g_z; // forward declaration for const global variable defined elsewhere
+
+// Initialized definition:
+int g_y(1);        // defines initialized global variable (external linkage, default since non-const)
+static int g_y(1); // defines initialized static global variable (internal linkage)
+const int g_y(1);  // defines initialized const (thus, static) global variable (internal linkage, default because const)
+
+// Initialized definition w/extern keyword:
+extern int g_w(1);       // defines initialized global variable (external linkage, extern keyword is redundant in this case, since non-const)
+extern const int g_w(1); // defines initialized const global variable (external linkage enforced by "extern", otherwise static since const)
---
+++ Chap_4/stringExample.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter your full name: ";
+    std::string name;
+    std::getline(std::cin, name); // read a full line of text into name
+    
+    std::cout << "Enter your age: ";
+    int age { 0 }; // age needs to be an integer, not a string, so we can do math with it
+    std::cin >> age;
+    
+    int letters = name.length(); // get number of letters in name (including spaces)
+    double agePerLetter = static_cast<double>(age) / letters; // static cast age to double to avoid integer division
+    std::cout << "You've lived " << agePerLetter << " years for each letter in your name.\n";
+    
+    return 0;
+}

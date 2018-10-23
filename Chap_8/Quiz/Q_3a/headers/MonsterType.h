d6ff9fd7b6cb81fe748053a036f6403e2291e829
Gabriele Pompa <gabriele.pompa@symmys.com>
2018-10-23 15:51:09 +0100

    Chap 8 - Q_3a: MonsterType

---
+++ Chap_8/Quiz/Q_3a/headers/MonsterType.h
@@ -0,0 +1,17 @@
+# ifndef MONSTER_TYPE_H
+# define MONSTER_TYPE_H
+
+enum MonsterType{
+
+    DRAGON,
+    GOBLIN,
+    OGRE,
+    ORC,
+    SKELETON,
+    TROLL,
+    VAMPIRE,
+    ZOMBIE,
+    MAX_MONSTER_TYPES
+};
+
+# endif
